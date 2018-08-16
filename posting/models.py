62970cbd435c519f3d807c662219b24cabc7b931
gpDA <geonpy@likelion.org>
2018-08-15 21:41:16 -0400

    image field for user

--- posting/models.py
+++ posting/models.py
@@ -3,6 +3,6 @@ from django.db import models
 
 class CustomUser(AbstractUser):
     age = models.CharField(max_length=2, default='10')
-
+    image = models.CharField(max_length=256, default='https://api.adorable.io/avatars/285/abott@adorable')
     def __str__(self):
         return self.email
--- posting/views.py
+++ posting/views.py
@@ -3,6 +3,7 @@ from django.views import generic
 from django.shortcuts import render
 #NOT YET USED from django.contrib.auth.decorators import login_required
 
+
 from .forms import CustomUserCreationForm
 
 def home(request):
