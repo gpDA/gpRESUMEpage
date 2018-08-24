9af4792f81fd2e236a69edc4e644ac1679b4268e
gpDA <geonpy@likelion.org>
2018-08-24 10:21:59 -0400

    change

--- posting/urls.py
+++ posting/urls.py
@@ -21,8 +21,7 @@ urlpatterns = [
     path('socials/', include('social_django.urls', namespace='social')),    
     path('ckeditor/', include('ckeditor_uploader.urls')),
     #path('activate/<slug:uidb64>/<slug:token>)/', views.activate, name='activate'),
-    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/',
-        views.activate, name='activate'),    
+    path('activate/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.activate, name='activate'),    
 
     #path('account_activation_sent/', views.ActivationSent, name='activationSent'),
 ]
