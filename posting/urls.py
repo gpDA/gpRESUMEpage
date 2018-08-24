e2e6c7dd230f2d4d3d8c44daa4970caa377d7653
gpDA <geonpy@likelion.org>
2018-08-24 10:15:51 -0400

    password reset

--- posting/urls.py
+++ posting/urls.py
@@ -6,17 +6,16 @@ urlpatterns = [
     path('', views.home, name='home'),
     path('signup/', views.SignUp, name='signup'),
     #password reset
-<<<<<<< HEAD
     #path('password_reset/', auth_views.password_reset, name='password_reset'),
     #path('password_reset/done/', auth_views.password_reset_done, name='password_reset_done'),
     #path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', auth_views.password_reset_confirm, name='password_reset_confirm'),
     #path('reset/done/', auth_views.password_reset_complete, name='password_reset_complete'),
-=======
+
     path('password_reset/', views.password_reset, name='password_reset'),
     path('password_reset/done/', views.password_reset_done, name='password_reset_done'),
     path('reset/(?P<uidb64>[0-9A-Za-z_\-]+)/(?P<token>[0-9A-Za-z]{1,13}-[0-9A-Za-z]{1,20})/', views.password_reset_confirm, name='password_reset_confirm'),
     path('reset/done/', views.password_reset_complete, name='password_reset_complete'),
->>>>>>> 10f4fda71efdceb6875e4ad60acb7a775158bf92
+
     
     #path('verified/', views.Actived, name='activated'),
     path('socials/', include('social_django.urls', namespace='social')),    
