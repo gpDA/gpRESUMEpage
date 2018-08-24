54a9260857fbcc9a5eb6c433e0131613a68f58e4
gpDA <geonpy@likelion.org>
2018-08-23 22:06:12 -0400

    urls.py

--- gpResume/urls.py
+++ gpResume/urls.py
@@ -3,7 +3,7 @@ from django.urls import path, include
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('postings/', include('posting.urls')),
-    path('', include('project.urls')),
+    path('', include('posting.urls')),
+    path('project/', include('project.urls')),
     path('', include('django.contrib.auth.urls')),    
 ]
