cd86f474777333dd60b43e70f8496254b344b561
gpDA <geonpy@likelion.org>
2018-08-26 19:01:17 -0400

    scrollspy and fadein div ready

--- static/js/index.js
+++ static/js/index.js
@@ -1,10 +1,11 @@
 $(document).ready(function(){
     //scroll event NAV
-
     var lastScrollTop = 0;
-    $(window).scroll(function(event){
+    $(window).scroll(function(){
+    //nav
     var scroll = $(this).scrollTop();
     if (scroll > lastScrollTop){
+        
         $("nav").removeClass("increase").addClass("decrease");
             $('.nav-items img').animate({height:"65px",width:"80px"}, 30);
             $('.nav-items').animate({lineHeight:"40px"}, 30);
@@ -15,26 +16,26 @@ $(document).ready(function(){
     }
     lastScrollTop = scroll;
     });
+});   
+$(document).on("scroll", function () {
+    var pageTop = $(document).scrollTop()
+    var pageBottom = pageTop + $(window).height()
+    var tags = $("section")
+  
+    for (var i = 0; i < tags.length; i++) {
+      var tag = tags[i]
+  
+      if ($(tag).position().top < pageBottom) {
+        $(tag).addClass("fadein")
+      } else {
+        $(tag).removeClass("fadein")
+      }
+    }
+  });
     
-    
-    $(window).on("load",function() {
-        $(window).scroll(function() {
-          var windowBottom = $(this).scrollTop() + $(this).innerHeight();
-          $(".fade").each(function() {
-            /* Check the location of each desired element */
-            var objectBottom = $(this).offset().top + $(this).outerHeight();
-            
-            /* If the element is completely within bounds of the window, fade it in */
-            if (objectBottom < windowBottom) { //object comes into view (scrolling down)
-              if ($(this).css("opacity")==0) {$(this).fadeTo(500,1);}
-            } else { //object goes out of view (scrolling up)
-              if ($(this).css("opacity")==1) {$(this).fadeTo(500,0);}
-            }
-          });
-        }).scroll(); //invoke scroll-handler on page-load
-      });
 
-});
+
+
 
 
 
